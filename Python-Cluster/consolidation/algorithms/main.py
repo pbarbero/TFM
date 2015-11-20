5071f504526c30f6a16b288ff281869a7851cdab
Pilar Barbero <pbarberoiriarte@gmail.com>
2015-11-20 18:56:46 +0100

    create tex with explanation of classes

---
+++ Python-Cluster/consolidation/algorithms/main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+#from consolidation import consolidationByDistance, consolidationByTime, consolidationEachNumber
+import consolidation
+from position import Position
+from datetime import datetime
+from db import connect_db 
+from djCluster import DjCluster
+
+def main():
+#	eps = 0.1
+#	p = Position("id", "recurso", -0.22707989811897278, -0.6720935106277466, 0, 112, "2015-02-17 08:00:06")
+#	q = Position("id", "recurso", -0.22707989811897278, -0.6720935106277466, 0, 112, "2015-02-17 08:00:06")
+#	print p.is_neighboorhoud(q, eps)
+
+	cur_sal = connect_db("posicionesSalvador")
+	cur_rio = connect_db("posicionesRio")
+
+	limit = 10
+	cmd = "SELECT * FROM posicionesgps LIMIT {0};".format(limit)
+	cur_sal.execute(cmd)
+
+	list_pos = []
+
+	for row in cur_sal.fetchall():
+		q = Position(row[0]
+			   , row[2]
+			   , row[3]
+			   , row[4]
+			   , row[5]
+			   , row[6]
+			   , row[10]
+		            )
+		list_pos.append(q)
+
+	# Consolidations
+	#result = consolidationByDistance(list_pos, 0.5)
+	#result = consolidationEachNumber(list_pos, 2)
+	#result = consolidationByTime(list_pos, 1)
+	result = consolidation.ConsolidationByDistance(list_pos, 2, 0, 1)
+
+#	print "EPIS LIST: Result -> " + str(len(result)) + " positions."
+#	for pos in result:
+#		print pos.toString()
+
+	# CLUSTERING
+	print "CLUSTERING"
+	print DjCluster(list_pos, 0, 0.5, 2, 0.50)
+	
+
+
+if __name__ == '__main__':
+	main()
---
+++ Python-Cluster/consolidation/position.py
@@ -0,0 +1,83 @@
+import math
+import time
+
+class Position:
+	"Represents GPS position"
+	def __init__(self, id, resource, lat, lon, speed, track, date):
+		self.id = id
+		self.resource = resource
+		self.lat = lat
+		self.lon = lon
+		self.speed = speed
+		self.track = track
+		self.date = date
+
+	"String Representation"
+	def toString(self):
+		return "id:{0}, recurso:{1}, latitud:{2}, longitud:{3}, velocidad:{4}, orientacion:{5}, fecha:{6}".format(self.id, self.resource, self.lat, self.lon, self.speed, self.track, self.date)
+
+	"Distance EU simple. Usage: p.distance(q)"
+	def distance_eu(self, q):
+		return math.sqrt((self.lat - q.lat)**2 + (self.lon - q.lon)**2)
+		
+	"Is it in neighboorhoud with radio eps?"
+	def is_in_neighborhoodByEUSimple(self, q, eps):
+		return self.distance_eu(q) < eps
+		
+	"Neighboorhoud EU involving speed module."
+	def is_in_neighborhoodByEURelativeSpeed(self, q, eps):
+		return self.distance_eu(q) < eps * self.speed
+		
+	"Neighboorhoud t0-reachable using distance EU and speed module."
+	def is_in_neighborhoodT0Reachable(self, q, t0):
+		return self.distance_eu(q) < t0 * self.speed
+
+	"Neighboorhoud EU involving speed and orientation."
+	def is_in_neighborhoodByEURelativeSpeedOrientation(self, q, eps):
+		# TODO :Obtain orientation from self.orientation
+		# return distance_eu(self, q) < eps * self.speed 
+		return NotImplemented
+
+
+	"Is it in neighboorhood by time?"
+	def is_neighboorhoudByTime(self, q, lapse):
+		foo = time.mktime(self.date.timetuple())
+		bar = time.mktime(q.date.timetuple())
+		return abs(foo - bar) < lapse
+
+	def isInCluster(self, cluster):
+		for q in cluster.points:
+			if self.id == q.id:
+				return True
+
+		return False
+
+
+class Cluster:
+	"Cluster of points"
+	def __init__(self, center, points):
+		self.center = center
+		self.points = points
+
+	"String Representation"
+	def toString(self):
+		return "Cluster centered in: " + str(self.center.toString())
+
+	"Cluster is density Joinable with list of clusters? Returns false if not, returns cluster if yes"
+	def isDensityJoinable(self, listClusters):
+		for cluster in listClusters:
+			for p in self.points:
+				if p.isInCluster(cluster):
+					return cluster
+
+		return None 
+
+	"Merge current cluster with another"
+	def mergeCluster(self, cluster)					:
+		for p in cluster.points:
+			if not p.isInCluster(cluster):
+				self.points.append(p)	
+
+		return self 
+				
+		
